96fa6b366b8faacfc479eefe8ab0bed565cf5ff5
Chandler Fang <chandlerfang@gmail.com>
2018-06-17 01:18:03 +1000

    Milestone 1: Getting started with the router

---
+++ src/app/not-found.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  template: '<h2>Page not found</h2>'
+})
+export class PageNotFoundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
